ea2619f22843c70717e5f87ad54027cd48535316
Lehman Garrison <lgarrison@cfa.harvard.edu>
2014-01-10 14:45:40 -0300

    Reads variable stars into pandas df

--- search_objects.py
+++ search_objects.py
@@ -4,9 +4,9 @@ import pandas as pd
 import numpy as np
 
 def main():
+	objects = pd.read_table('Variables_var', header=None, names=['obj', 'ra', 'dec', 'N', 'mean', 'median', 'rms', 'median_err', 'skewness', 'chi2', 'sigma', 'dr_rms'])
 	
-	
-	
+	# use astroquery here, using objects['ra'] and objects['dec'] as the positions
 	
 	return 0
 
